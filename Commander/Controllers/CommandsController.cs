55f755dae11b0a7df853fed9c873ef04a67773b6
Filip <filip.lagerqvist@hotmail.se>
2026-01-17 15:25:48 +0100

    Adding 2 endpoints + dependency injection.

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers;
+
+[Route("api/commands")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    private readonly ICommanderRepo _repository;
+
+    public CommandsController(ICommanderRepo repo)
+    {
+        _repository = repo;
+    }
+
+    // GET api/commands
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+        var commandItems = _repository.GetAllCommands();
+
+        return Ok(commandItems);
+    }
+
+    // GET api/commands/{id}
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+        var commandItem = _repository.GetCommandById(id);
+
+        return Ok(commandItem);
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,23 @@
+using Commander.Models;
+
+namespace Commander.Data;
+
+public class MockCommanderRepo : ICommanderRepo
+{
+    public IEnumerable<Command> GetAllCommands()
+    {
+        var commands = new List<Command>
+        {
+            new Command{ Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+            new Command{ Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Chopping Board" },
+            new Command{ Id = 2, HowTo = "Make a cup of coffee", Line = "Place ground coffee in machine  & add water", Platform = "Coffee machine & Cup" },
+        };
+
+        return commands;
+    }
+
+    public Command GetCommandById(int id)
+    {
+        return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+    }
+}
