b073e84fc04a8d75145f69eb7a74f1814282e51f
Filip <filip.lagerqvist@hotmail.se>
2026-01-18 15:51:00 +0100

    Added Nuget packages.

--- Commander/obj/Debug/net10.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net10.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e932d177f0e3daca0e153218ffbbea173e93cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f755dae11b0a7df853fed9c873ef04a67773b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
